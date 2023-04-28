626b7f091ac53bb050813137acdbbaea79d05808
Ilya Borshchevskiy <ilya.borshchevskiy@gmail.com>
2023-04-28 12:50:59 +0300

    added i18n

--- src/main/java/ru/investportfolio/config/WebConfig.java
+++ src/main/java/ru/investportfolio/config/WebConfig.java
@@ -1,13 +1,21 @@
 package ru.investportfolio.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import ru.investportfolio.database.entity.CashAction;
 import ru.investportfolio.database.entity.DealType;
+
+import java.util.Locale;
 
 @Configuration
 @Slf4j
@@ -24,6 +32,10 @@ public class WebConfig implements WebMvcConfigurer {
         resourceHandlerRegistry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
     }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 
     public static class StringToCashActionConverter implements Converter<String, CashAction> {
         @Override
@@ -37,5 +49,19 @@ public class WebConfig implements WebMvcConfigurer {
         public DealType convert(String source) {
             return DealType.valueOf(source.toUpperCase());
         }
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setDefaultLocale(Locale.ENGLISH);
+        resolver.setCookieName("lang");
+        return resolver;
+    }
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
     }
 }
